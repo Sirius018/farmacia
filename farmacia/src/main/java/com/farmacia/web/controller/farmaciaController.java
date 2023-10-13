de5205ff0838b620b2bd4a94d1cf5c773e95a98b
Sirius018 <betochirakudai@gmail.com>
2023-10-12 23:18:28 -0500

    front usuario semi-complet + home post login

--- farmacia/src/main/java/com/farmacia/web/controller/farmaciaController.java
+++ farmacia/src/main/java/com/farmacia/web/controller/farmaciaController.java
@@ -1,14 +1,19 @@
 package com.farmacia.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class farmaciaController {
 	
 	
-	
-	public String home() {
+	@GetMapping("/")
+	public String index() {
 		return "index";
 	}
 
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
 }
